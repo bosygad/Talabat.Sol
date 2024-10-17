f8742c853b467b139fa57d4c20299f9e777e417f
bossyGad <bosygad179@gmail.com>
2024-10-18 02:17:46 +0300

    Product Configurations

--- Talabat.Repository/Data/BDContext/StoreDbContext.cs
+++ Talabat.Repository/Data/BDContext/StoreDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.Core.Entities;
@@ -14,7 +15,12 @@ namespace Talabat.Repository.Data.BDContext
         {
             
         }
-      public DbSet<Product> products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
     }
