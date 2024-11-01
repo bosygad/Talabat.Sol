ce9594217ba7df724163d6b201216ec95cdb986a
bossyGad <bosygad179@gmail.com>
2024-11-02 00:51:18 +0200

    Error Types at API

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -13,7 +13,7 @@ namespace Talabat.APIs
         public static async Task  Main(string[] args)
         {
 
-            Console.WriteLine("Session 03");
+          
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
